43b36d0c051ba33fe6b3884171865c29507fa7fa
Bryce Graf <bag5620@psu.edu>
2020-10-01 01:35:50 +0000

    f

--- quiz1/main.c
+++ quiz1/main.c
@@ -1,7 +1,11 @@
 // Author: 
 #include <stdio.h>
+#include <readline.h>
 #include "quiz1.h"
 
 int main(void) {
+  n = int(readline("Enter an int: "))
+  base = int(readline("Enter an int: "))
+  printf("answer is %f", digit_sum_base(n, base))
   return 0;
 }
--- quiz1/quiz1.c
+++ quiz1/quiz1.c
@@ -1,5 +1,12 @@
 // Author: 
 #include "quiz1.h"
 int digit_sum_base(int n, int base) {
+   if (n < 10):
+    return n
+  else:
+    return n % base + digit_sum_base(n // 10)
+ 
+
+
   return 0;
 }
