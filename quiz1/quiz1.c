229a38d2c69afc2a6f76d00c8cd80c4b4a9e2b4e
Bryce Graf <bag5620@psu.edu>
2020-10-01 02:12:45 +0000

    ;;

--- quiz1/quiz1.c
+++ quiz1/quiz1.c
@@ -4,8 +4,8 @@
 
 int digit_sum_base(int n, int base) {
    if (n < 10):
-    return n % base
+    return n % base;
   else:
-    return n % base + digit_sum_base(n / 10)
+    return n % base + digit_sum_base(n / 10);
   ;
 }
